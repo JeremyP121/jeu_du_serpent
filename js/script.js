5a3f133b4f4644188f9c1fe6324968e7facfebaa
Jeremy Perreault <jejedragon56rgmail.com>
2020-11-20 09:06:53 -0500

    Ajout des classes dans le javascript pour définir les objets.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,35 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
+    //Le jeu           Ce commentaire va nous permettre de mettre tout le code lié au jeu au même endroit.
+    //La classe va contenir tout ce qui va caractériser un objet. Dans ce cas-ci l'objet est le jeu.
+    class Jeu{
+
+        //Permet d'initialiser des variables ou de faire des fonctions directement.
+        constructor(){
+            console.log("Création du jeu");
+        }
+
+    }
+
+    //Le serpent           Ce commentaire va nous permettre de mettre tout le code lié au serpent au même endroit.
+    class Serpent{
+
+        //Permet d'initialiser des variables ou de faire des fonctions directement.
+        constructor(){
+            console.log("Création du serpent");
+        }
+
+    }
+
+    //La pomme           Ce commentaire va nous permettre de mettre tout le code lié à la pomme au même endroit.
+    class Pomme{
+
+        //Permet d'initialiser des variables ou de faire des fonctions directement.
+        constructor(){
+            console.log("Création de la pomme");
+        }
+
+    }
 
 });
